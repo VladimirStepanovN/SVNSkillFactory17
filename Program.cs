30601c543264540b53dd6a25d63e3be14274c4f4
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-06-13 00:35:05 +0500

    Задание 7.1.5.

--- Program.cs
+++ Program.cs
@@ -4,21 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
-    class Employee
-    {
-        public string Name;
-        public int Age;
-        public int Salary;
-    }
-    class ProjectManage : Employee
-    {
-        public string ProjectName;
-    }
-    class Developer : Employee
-    {
-        public string ProgrammingLanguage;
-    }
+    class Food { }
+    class Fruit : Food { }
+    class Vegetable : Food { }
+    class Apple : Fruit { }
+    class Banana : Fruit { }
+    class Pear : Fruit { }
+    class Potato : Vegetable { }
+    class Carrot : Vegetable { }
 }
