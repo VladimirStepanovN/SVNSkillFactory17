d696931402c8d70d2897318a8b8bdd2b6dd46067
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-06-15 01:00:40 +0500

    Задание 7.2.3.

--- Program.cs
+++ Program.cs
@@ -4,28 +4,22 @@
     {
         static void Main(string[] args)
         {
-
+            DerivedClass dc = new();
+            dc.Display();
         }
     }
     class BaseClass
     {
-        protected string Name;
-        public BaseClass(string name)
+        public virtual void Display()
         {
-            Name = name;
+            Console.WriteLine("Метод класса BaseClass");
         }
     }
     class DerivedClass : BaseClass
     {
-        public string Description;
-        public int Counter;
-        public DerivedClass(string name, string description) : base(name)
+        public override void Display()
         {
-            Description = description;
-        }
-        public DerivedClass(string name, string description, int counter) : this(name, description)
-        {
-            Counter = counter;
+            Console.WriteLine("Метод класса DerivedClass");
         }
     }
 }
