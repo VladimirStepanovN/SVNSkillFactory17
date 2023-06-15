9741fd12b02be4cb010554378f422e875595fcb6
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-06-15 20:35:59 +0500

    Задание 7.2.4.

--- Program.cs
+++ Program.cs
@@ -5,21 +5,35 @@
         static void Main(string[] args)
         {
             DerivedClass dc = new();
-            dc.Display();
+            dc.Counter = 10;
+            Console.WriteLine(dc.Counter);
+            dc.Counter = 0;
+            Console.WriteLine(dc.Counter);
+            dc.Counter = -1;
+            Console.WriteLine(dc.Counter);
         }
-    }
-    class BaseClass
-    {
-        public virtual void Display()
+        class BaseClass
         {
-            Console.WriteLine("Метод класса BaseClass");
+            public virtual int Counter
+            {
+                get;
+                set;
+            }
         }
-    }
-    class DerivedClass : BaseClass
-    {
-        public override void Display()
+        class DerivedClass : BaseClass
         {
-            Console.WriteLine("Метод класса DerivedClass");
+            private int counter;
+            public override int Counter
+            {
+                get
+                {
+                    return counter;
+                }
+                set
+                {
+                    if (value >= 0) counter = value;
+                }
+            }
         }
     }
 }
