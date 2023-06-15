f1adcb8a2ce854eca3667829e783819cb7c13df8
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-06-15 23:53:18 +0500

    Задание 7.6.12.

--- Program.cs
+++ Program.cs
@@ -5,17 +5,30 @@
         static void Main(string[] args)
         {
         }
-        class Engine { }
+        abstract class Engine { }
         class ElectricEngine : Engine { }
         class GasEngine : Engine { }
-        class CarPart { }
+        abstract class CarPart { }
         class Battery : CarPart { }
         class Differential : CarPart { }
         class Wheel : CarPart { }
-        class Car<TEngine> where TEngine : Engine
+        abstract class Car<TEngine> where TEngine : Engine
         {
             public TEngine Engine;
-            public virtual void ChangePart<TPart>(TPart newPart) where TPart : CarPart
+
+            public abstract void ChangePart<TPart>(TPart newPart) where TPart : CarPart;
+        }
+
+        class ElectricCar : Car<ElectricEngine>
+        {
+            public override void ChangePart<TPart>(TPart newPart)
+            {
+
+            }
+        }
+        class GasCar : Car<GasEngine>
+        {
+            public override void ChangePart<TPart>(TPart newPart)
             {
             }
         }
