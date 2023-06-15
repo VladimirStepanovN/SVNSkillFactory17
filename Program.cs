738ad16c12a43d0692a5db8673279f32fad08b07
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-06-15 23:20:18 +0500

    Задание 7.5.3.

--- Program.cs
+++ Program.cs
@@ -4,13 +4,18 @@
     {
         static void Main(string[] args)
         {
-
+            int num1 = 3;
+            int num2 = 58;
+            Helper.Swap(ref num1, ref num2);
+            Console.WriteLine(num1); //58
+            Console.WriteLine(num2); //3
         }
-        class Obj
+        class Helper
         {
-            public string Name;
-            public string Description;
-            public static int MaxValue = 2000;
+            public static void Swap(ref int one, ref int two)
+            {
+                (one, two) = (two, one);
+            }
         }
     }
 }
