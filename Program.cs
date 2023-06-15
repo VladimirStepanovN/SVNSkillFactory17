ea9bc9715162b7c2ed8b21e72a8332ad5de5490d
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-06-15 23:51:39 +0500

    Задание 7.6.10.

--- Program.cs
+++ Program.cs
@@ -12,10 +12,10 @@
         class Battery : CarPart { }
         class Differential : CarPart { }
         class Wheel : CarPart { }
-        class Car<T1> where T1 : Engine
+        class Car<TEngine> where TEngine : Engine
         {
-            public T1 Engine;
-            public virtual void ChangePart<T2>(T2 newPart) where T2 : CarPart
+            public TEngine Engine;
+            public virtual void ChangePart<TPart>(TPart newPart) where TPart : CarPart
             {
             }
         }
