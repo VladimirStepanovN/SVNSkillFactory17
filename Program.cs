f88b49b6f74b1f733d07032ac8f59fcd1039b377
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-06-15 23:38:48 +0500

    Задание 7.6.2.

--- Program.cs
+++ Program.cs
@@ -4,26 +4,12 @@
     {
         static void Main(string[] args)
         {
-            int num1 = 7;
-            int num2 = -13;
-            int num3 = 0;
-            Console.WriteLine(num1.GetNegative()); //-7
-            Console.WriteLine(num1.GetPositive()); //7
-            Console.WriteLine(num2.GetNegative()); //-13
-            Console.WriteLine(num2.GetPositive()); //13
-            Console.WriteLine(num3.GetNegative()); //0
-            Console.WriteLine(num3.GetPositive()); //0
         }
-    }
-    static class IntExtensions
-    {
-        public static int GetNegative(this int number)
+        class ElectricEngine { }
+        class GasEngine { }
+        class Car<T>
         {
-            return (number > 0) ? -number : number;
-        }
-        public static int GetPositive(this int number)
-        {
-            return (number < 0) ? -number : number;
+            public T Engine;
         }
     }
 }
